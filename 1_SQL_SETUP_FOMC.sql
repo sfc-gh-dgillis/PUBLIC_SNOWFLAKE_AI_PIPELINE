34e613fb69228ba6c810e56b8355971ce8baac84
sfc-gh-dgillis <daniel.gillis@snowflake.com>
2025-03-27 17:10:06 -0700

    Add Create Stream on Directory Table
    also:
    
    - Update README.md instructions
    - add gitignore
    - add minor format, spelling and comment changes
    - remove .gitkeep files for directories with files

--- 1_SQL_SETUP_FOMC.sql
+++ 1_SQL_SETUP_FOMC.sql
@@ -1,74 +1,87 @@
 --authors: John Heisler & Garrett Frere
+
+USE ROLE SYSADMIN;
+
 --Create our warehouse
-CREATE warehouse GEN_AI_FSI_WH warehouse_size = 'medium';
+CREATE OR REPLACE WAREHOUSE gen_ai_fsi_wh
+    WAREHOUSE_SIZE = 'medium';
 
 --Create our database
-CREATE OR REPLACE DATABASE GEN_AI_FSI;
+CREATE OR REPLACE DATABASE gen_ai_fsi;
 
 --create your schema
-CREATE OR REPLACE SCHEMA GEN_AI_FSI.FOMC;
+CREATE OR REPLACE SCHEMA gen_ai_fsi.fomc;
 
---create stage fed_LOGIC;
-CREATE OR REPLACE STAGE FED_LOGIC DIRECTORY = (ENABLE = TRUE);
+--create stage fed_logic;
+CREATE OR REPLACE STAGE gen_ai_fsi.fomc.fed_logic
+    DIRECTORY = (ENABLE = TRUE);
 
---create stage fed;
-CREATE OR REPLACE STAGE FED_PDF DIRECTORY = (ENABLE = TRUE);
+--create stage fed_pdf;
+CREATE OR REPLACE STAGE gen_ai_fsi.fomc.fed_pdf
+    DIRECTORY = (ENABLE = TRUE);
 
--- Create a sequence
-CREATE OR REPLACE SEQUENCE FED_PDF_FULL_TEXT_SEQUENCE;
-CREATE OR REPLACE SEQUENCE FED_PDF_CHUNK_SEQUENCE;
+-- create a stream on the directory
+CREATE OR REPLACE STREAM GEN_AI_FSI.FOMC.FOMC_STREAM on DIRECTORY(gen_ai_fsi.fomc.fed_pdf);
+
+-- Create sequences
+CREATE OR REPLACE SEQUENCE gen_ai_fsi.fomc.fed_pdf_full_text_sequence;
+CREATE OR REPLACE SEQUENCE gen_ai_fsi.fomc.fed_pdf_chunk_sequence;
 
 --store model data for meta analysis
-CREATE OR REPLACE TABLE MODELS (model varchar, context_window int);
+CREATE OR REPLACE TABLE gen_ai_fsi.fomc.models (
+    model          VARCHAR,
+    context_window INT
+);
 
---isnert values into models table 
-INSERT INTO MODELS
-    VALUES
-        --('snowflake-arctic', 4096) 
-        ('mistral-large', 32000),
-        ('reka-flash', 100000),
-        ('reka-core', 32000),
-        ('jamba-instruct', 256000), 
-        ('mixtral-8x7b', 32000),
-        ('llama2-70b-chat', 4096),
-        ('llama3-8b', 8000),
-        ('llama3-70b', 8000),
-        ('llama3.1-8b', 128000), 
-        ('llama3.1-70b', 128000), 
-        ('llama3.1-405b', 128000), 
-        ('mistral-7b', 32000),
-        ('gemma-7b', 8000);
+--insert values into models table
+INSERT INTO gen_ai_fsi.fomc.models (model, context_window)
+VALUES ('mistral-large', 32000),
+       ('reka-flash', 100000),
+       ('reka-core', 32000),
+       ('jamba-instruct', 256000),
+       ('mixtral-8x7b', 32000),
+       ('llama2-70b-chat', 4096),
+       ('llama3-8b', 8000),
+       ('llama3-70b', 8000),
+       ('llama3.1-8b', 128000),
+       ('llama3.1-70b', 128000),
+       ('llama3.1-405b', 128000),
+       ('mistral-7b', 32000),
+       ('gemma-7b', 8000);
 
 --create our full text table
-CREATE OR REPLACE TABLE PDF_FULL_TEXT (
-	ID NUMBER(19,0),
-	RELATIVE_PATH VARCHAR(16777216),
-	SIZE NUMBER(38,0),
-	LAST_MODIFIED TIMESTAMP_TZ(3),
-	MD5 VARCHAR(16777216),
-	ETAG VARCHAR(16777216),
-	FILE_URL VARCHAR(16777216),
-	FILE_TEXT VARCHAR(16777216),
-	FILE_DATE DATE,
-    SENTIMENT VARCHAR(16777216)
+CREATE OR REPLACE TABLE gen_ai_fsi.fomc.pdf_full_text (
+    id            NUMBER(19, 0),
+    relative_path VARCHAR(16777216),
+    size          NUMBER(38, 0),
+    last_modified TIMESTAMP_TZ(3),
+    md5           VARCHAR(16777216),
+    etag          VARCHAR(16777216),
+    file_url      VARCHAR(16777216),
+    file_text     VARCHAR(16777216),
+    file_date     DATE,
+    sentiment     VARCHAR(16777216)
 );
 
-CREATE OR REPLACE TABLE GEN_AI_FSI.FOMC.PDF_CHUNKS (
-	ID NUMBER(19,0),
-    FULL_TEXT_FK NUMBER(19,0),
-	RELATIVE_PATH VARCHAR(16777216),
-    FILE_DATE DATE,
-	CHUNK VARCHAR(16777216)
+CREATE OR REPLACE TABLE gen_ai_fsi.fomc.pdf_chunks (
+    id            NUMBER(19, 0),
+    full_text_fk  NUMBER(19, 0),
+    relative_path VARCHAR(16777216),
+    file_date     DATE,
+    chunk         VARCHAR(16777216)
 );
 
---In order to go to the public internet and download the PDFs, we need a network rule and network access integration.
---create the network rule
-CREATE OR REPLACE NETWORK RULE FED_RESERVE
+-- In order to go to the public internet and download the PDFs,
+-- we need a network rule and external access integration.
+
+-- create the network rule
+CREATE OR REPLACE NETWORK RULE gen_ai_fsi.fomc.fed_reserve
   MODE = EGRESS
   TYPE = HOST_PORT
   VALUE_LIST = ('www.federalreserve.gov');
 
---add the network rule to external access integration
-CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION FED_RESERVE_ACCESS_INTEGRATION
+-- add the network rule to external access integration
+USE ROLE ACCOUNTADMIN;
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION fed_reserve_access_integration
   ALLOWED_NETWORK_RULES = (FED_RESERVE)
-  ENABLED = true;
+  ENABLED = TRUE;
--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # Cortex AI for Financial Services
 
-The intent of these assets is both instructional and functional. In FSI_Cortex_AI_Pipeline.ipynb and FSI_Cortex_Search.ipynb
-we aim to demonstrate the art of the possible with two of our features, Cortex Complete and Cortex Search, respectively.
+### Author
+John Heisler
 
-In FSI_Cortex_AI_Pipeline_Industrialization.ipynb, we take the functional elements built in FSI_Cortex_AI_Pipeline.ipynb and industralize them toward an enterprise-ready AI pipeline.
+The intent of these assets is both instructional and functional. In `FSI_Cortex_AI_Pipeline.ipynb` and `FSI_Cortex_Search.ipynb` we aim to demonstrate the art of the possible with two of our features, [Cortex Complete](https://docs.snowflake.com/en/sql-reference/functions/complete-snowflake-cortex) and [Cortex Search](https://docs.snowflake.com/en/user-guide/snowflake-cortex/cortex-search/cortex-search-overview), respectively.
+
+In `FSI_Cortex_AI_Pipeline_Industrialization.ipynb`, we take the functional elements built in `FSI_Cortex_AI_Pipeline.ipynb` and industrialize them toward an enterprise-ready AI pipeline.
+
+## Presentation / Recording
+
+Presentation URL: https://docs.google.com/presentation/d/1NASynbIFFX4SfvkdwQIi3Y9l5v4o1iUA1n1EIn0DGmI/edit#slide=id.p
+
+Recordings URL: https://snowflake.zoom.us/rec/share/decjfFGdLiWOPxny8iJG9d6mEWme_MgNLYTrCgvz_tQViYKs1gQsQ03n6BaVG-ZP.gMehFA0WUSZr899m
+
+> Recordings PWD: `4N=a1!Kj`
 
 ## Suggested Learning Path
-1. Run Set Up SQL Script
-2. Load FOMC documents into your stage (https://snowflakewiki.medium.com/loading-files-into-stage-through-snowflake-ui-the-complete-guide-321b135f6175)
-3. FSI_Cortex_AI_Pipeline.ipynb
-4. FSI_Cortex_Search.ipynb
-5. Read our Medium Article on AI Pipelines (https://medium.com/snowflake/ai-infused-pipelines-with-snowflake-cortex-6a7954f2078d) and follow along with FSI_Cortex_AI_Pipeline_Industrialization.ipynb
 
-I hope you learn as much as I did builidng these! 
+1. Run Set Up SQL Script `1_SQL_SETUP_FOMCl.sql`
+	1. **Note**: `SYSADMIN` or a role with `CREATE WAREHOUSE`, `CREATE DATABASE` privileges are required. For the final statement `CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION`, the `ACCOUNTADMIN` role is required.
+2. Load FOMC documents in the `FOMC_DOCS` directory into the `fed_logic` stage via snowsight.
+	1. See [Loading Files into Stage through Snowflake UI — The Complete Guide](https://snowflakewiki.medium.com/loading-files-into-stage-through-snowflake-ui-the-complete-guide-321b135f6175)
+3. Step through `FSI_Cortex_AI_Pipeline.ipynb`
+4. Step through `FSI_Cortex_Search.ipynb`
+5. Read our [AI-Infused Pipelines with Snowflake Cortex Medium Article](https://medium.com/snowflake/ai-infused-pipelines-with-snowflake-cortex-6a7954f2078d) and step through `FSI_Cortex_AI_Pipeline_Industrialization.ipynb`
 
+I hope you learn as much as I did building these!
+  
 John Heisler
